b962824c8ac811c29441fd35b28b6a9df2010f84
vanshikabansal3 <112400083+vanshikabansal3@users.noreply.github.com>
2022-08-30 15:46:50 +0530

    DAY 10

---
+++ DS I/Day 10/94- Binary Tree Inorder Traversa.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+         List<Integer> res=new ArrayList<>();
+        
+        inorderTraversal(root,res);
+        return res;
+    }
+    public void inorderTraversal(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        
+        inorderTraversal(root.left,list);
+        list.add(root.val);
+        inorderTraversal(root.right,list);
+    }
+}
